5c8df1f118e14b45aa87d2d7d5bc7692303fd93f
jeexan2 <jeexan2@gmail.com>
2023-03-20 02:40:04 +0700

    trie + geohash requirement

--- src/main/scala/geohash/GeoHash.scala
+++ src/main/scala/geohash/GeoHash.scala
@@ -8,4 +8,17 @@ class GeoHash  {
   // subdivided by splitting it at the midpoint of its longest side. The
   // resulting sub-boxes are then subdivided recursively, until the desired
   // precision is reached.
+
+  //The geohash is a string of base32 characters. The characters are ordered
+  // from left to right, and the first character represents the most significant
+  // bit of the hash. The last character represents the least significant bit.
+  //  The bits are ordered from left to right, and the first bit represents the
+  //  most significant bit of the hash. The last bit represents the least
+  //  significant bit.
+
+  //Implementation of geohash in scala
+  //http://en.wikipedia.org/wiki/Geohash
+  //http://en.wikipedia.org/wiki/Geohash#Algorithm_and_example
+
+
 }
---
+++ src/main/scala/trie/Trie.scala
@@ -0,0 +1,10 @@
+package trie
+//Trie Data Structure Required for AutoComplete
+//Trie is a tree like data structure which is used to store the dictionary of words
+//Trie is used to store the words in a sorted manner
+//Trie is used to search the words in O(m) time where m is the length of the word
+//Trie is used to store the words in O(m) space where m is the length of the word
+//Trie is used to store the words in a sorted manner
+class Trie {
+
+}
